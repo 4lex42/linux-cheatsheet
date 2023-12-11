78ec34e189a046b3e5a2cd5c1865a232a397d885
Alex <147047219+4lex42@users.noreply.github.com>
2023-12-11 08:45:26 +0100

    Add files via upload

---
+++ TP07/test_TP07.py
@@ -0,0 +1,39 @@
+from unittest import TestCase
+
+
+class TestFraction(TestCase):
+    def test_numerator(self):
+        self.fail()
+
+    def test_denominator(self):
+        self.fail()
+
+    def test_numerator_simplifiee(self):
+        self.fail()
+
+    def test_denominator_simplifiee(self):
+        self.fail()
+
+    def test_as_mixed_number(self):
+        self.fail()
+
+    def test_is_greater(self):
+        self.fail()
+
+    def test_is_smaller(self):
+        self.fail()
+
+    def test_is_zero(self):
+        self.fail()
+
+    def test_is_integer(self):
+        self.fail()
+
+    def test_is_proper(self):
+        self.fail()
+
+    def test_is_unit(self):
+        self.fail()
+
+    def test_is_adjacent_to(self):
+        self.fail()
